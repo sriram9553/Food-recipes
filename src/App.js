8cef3aa3a36ad9e3c9c19590cbb11acf674ac1d3
sriram9553 <moparthi.sriram.124@gmail.com>
2021-10-01 16:39:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useState} from 'react';
+import Recipe from './Recipe';
 
-function App() {
-  return (
+const App = () =>{
+
+const APP_ID = "a71faa37";
+const APP_KEY = "7b1d0bb78650acd87454d78da58933e5";
+const [recipes,setRecipes] = useState([]);
+const [search,setSearch] = useState('');
+const [query,setquery] = useState('chiken');
+useEffect(() => {
+  getRecipes()
+}, [query])
+ 
+const getRecipes = async () =>{
+  const Response = await fetch(`http://www.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await Response.json();
+  setRecipes(data.hits);
+
+}
+
+const updateSearch = (e) =>{
+  setSearch(e.target.value)
+}
+
+const getSearch = (e) =>{
+  e.preventDefault();
+  setquery(search);
+}
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange = {updateSearch} />
+        <button className="search-button" type="sudmit" >Search</button>
+      </form >
+      <div className="recipes">
+      {recipes.map(recipe =>(
+        <Recipe
+        key = {recipe.recipe.label} 
+        title = {recipe.recipe.label} 
+        calories = {recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        ingredients = {recipe.recipe.ingredients}  />
+      ))}
+      </div>
     </div>
   );
 }
---
+++ src/Recipe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Recipe.css";
+
+const Recipe = ({title,calories,image,ingredients}) =>{
+    return(
+        <div className="recipe">
+           <h1>{title}</h1>
+           <ol>
+             {ingredients.map(ingredient =>(
+                 <li>{ingredient.text}</li>
+             ))}
+           </ol>
+           <p>{calories}</p>
+           <img className="img" src = {image} alt=""/>
+        </div>
+    )
+};
+  export default Recipe;
